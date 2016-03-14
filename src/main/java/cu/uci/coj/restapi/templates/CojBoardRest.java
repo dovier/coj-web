be3df0139ac47cb18653fb84336d216414abf389
Cesar A Glez <caglez@estudiantes.uci.cu>
2016-03-14 11:28:57 -0400

    Todos los metdodos publicos termiandos

--- src/main/java/cu/uci/coj/restapi/templates/CojBoardRest.java
+++ src/main/java/cu/uci/coj/restapi/templates/CojBoardRest.java
@@ -12,17 +12,61 @@ package cu.uci.coj.restapi.templates;
 public class CojBoardRest {
     String name;
     String site;
-    String status;
+    String urlcontest;
     String start;
     String end;
 
-    public CojBoardRest(String name, String site, String status, String start, String end) {
+    public CojBoardRest(String name, String site, String urlcontest, String start, String end) {
         this.name = name;
         this.site = site;
-        this.status = status;
+        this.urlcontest = urlcontest;
         this.start = start;
         this.end = end;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getUrlcontest() {
+        return urlcontest;
+    }
+
+    public void setUrlcontest(String urlcontest) {
+        this.urlcontest = urlcontest;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+    
+    
+
+    
     
     
 }
