68d8f596ff8d0f0a188a616988403db053d199eb
jorge.amado <darksoul.uci@gmail.com>
2016-11-22 17:46:10 -0300

    selected changes for local deployment.

--- src/main/java/cu/uci/coj/config/DatabaseConfiguration.java
+++ src/main/java/cu/uci/coj/config/DatabaseConfiguration.java
@@ -1,10 +1,9 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * To change this template, choose Tools | Templates and open the template in the editor.
  */
 package cu.uci.coj.config;
 
-import java.beans.PropertyVetoException;
+import java.sql.Driver;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -15,66 +14,60 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
-@PropertySource({
-		"file:/home/coj/config.properties",
-		"classpath:cu/uci/coj/config/sql/contest_awards.properties",
-		"classpath:cu/uci/coj/config/sql/notifications.properties",
-		"classpath:cu/uci/coj/config/sql/postgres.properties",
-		"classpath:cu/uci/coj/config/sql/mail.properties",
-		"classpath:cu/uci/coj/config/sql/entries.properties",
-		"classpath:cu/uci/coj/config/sql/json.api.properties",
-		"classpath:cu/uci/coj/config/sql/achievements.properties",
-		"classpath:cu/uci/coj/config/sql/events.properties",
-		"classpath:cu/uci/coj/config/sql/corrections.properties",
-		"classpath:cu/uci/coj/config/sql/teamanalyzer.properties",
-		"classpath:cu/uci/coj/config/sql/recommender.properties",
-		"classpath:cu/uci/coj/config/sql/wboard.properties",
-		"classpath:cu/uci/coj/config/sql/ws.properties" })
+@PropertySource({ "classpath:cu/uci/coj/config/config.properties", "classpath:cu/uci/coj/config/sql/contest_awards.properties",
+        "classpath:cu/uci/coj/config/sql/notifications.properties", "classpath:cu/uci/coj/config/sql/postgres.properties",
+        "classpath:cu/uci/coj/config/sql/mail.properties", "classpath:cu/uci/coj/config/sql/entries.properties",
+        "classpath:cu/uci/coj/config/sql/json.api.properties", "classpath:cu/uci/coj/config/sql/achievements.properties",
+        "classpath:cu/uci/coj/config/sql/events.properties", "classpath:cu/uci/coj/config/sql/corrections.properties",
+        "classpath:cu/uci/coj/config/sql/teamanalyzer.properties", "classpath:cu/uci/coj/config/sql/recommender.properties",
+        "classpath:cu/uci/coj/config/sql/wboard.properties", "classpath:cu/uci/coj/config/sql/ws.properties" })
 public class DatabaseConfiguration {
 
-	@Resource
-	protected Environment env;
-	private ComboPooledDataSource bean = null;
+    @Resource
+    protected Environment         env;
+    private ComboPooledDataSource bean = null;
 
-	@Bean
-	public DataSource ds() {
+    @Bean
+    public DataSource ds() {
 
-		if (bean == null) {
-			bean = new ComboPooledDataSource();
-			try {
-				bean.setDriverClass("org.postgresql.Driver");
-				bean.setJdbcUrl(env.getProperty("db.url"));
-				bean.setUser(env.getProperty("db.user"));
-				bean.setPassword(env.getProperty("db.password"));
+        if (bean == null) {
+            bean = new ComboPooledDataSource();
+            try {
+                bean.setDriverClass("org.postgresql.Driver");
+                bean.setJdbcUrl(env.getProperty("db.url"));
+                bean.setUser(env.getProperty("db.user"));
+                bean.setPassword(env.getProperty("db.password"));
 
-				bean.setMaxPoolSize(10);
-				bean.setMaxIdleTime(300);
-				bean.setMaxConnectionAge(600);
-				bean.setIdleConnectionTestPeriod(1000);
-				bean.setMaxStatementsPerConnection(3);
-				bean.setAutoCommitOnClose(true);
-				bean.setCheckoutTimeout(5000);
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-		return bean;
-	}
+                bean.setMaxPoolSize(10);
+                bean.setMaxIdleTime(300);
+                bean.setMaxConnectionAge(600);
+                bean.setIdleConnectionTestPeriod(1000);
+                bean.setMaxStatementsPerConnection(3);
+                bean.setAutoCommitOnClose(true);
+                bean.setCheckoutTimeout(60000);
+            }
+            catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return bean;
+    }
 
-	@Bean
-	// @DependsOn("flyway")
-	public JdbcTemplate jdbcTemplate() {
-		JdbcTemplate bean = new JdbcTemplate();
-		bean.setDataSource(ds());
-		return bean;
-	}
+    @Bean
+    // @DependsOn("flyway")
+    public JdbcTemplate jdbcTemplate() {
+        JdbcTemplate bean = new JdbcTemplate();
+        bean.setDataSource(ds());
+        return bean;
+    }
 
-	@Bean
-	public DataSourceTransactionManager transactionManager() {
-		return new DataSourceTransactionManager(ds());
-	}
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(ds());
+    }
 }
--- src/main/java/cu/uci/coj/restapi/controller/RestProblemsController.java
+++ src/main/java/cu/uci/coj/restapi/controller/RestProblemsController.java
@@ -7,10 +7,30 @@ package cu.uci.coj.restapi.controller;
 
 
 
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.wordnik.swagger.annotations.ApiOperation;
 import com.wordnik.swagger.annotations.ApiParam;
 import com.wordnik.swagger.annotations.ApiResponse;
 import com.wordnik.swagger.annotations.ApiResponses;
+
 import cu.uci.coj.dao.ContestDAO;
 import cu.uci.coj.dao.ProblemDAO;
 import cu.uci.coj.dao.RecommenderDAO;
@@ -27,23 +47,6 @@ import cu.uci.coj.restapi.utils.ErrorUtils;
 import cu.uci.coj.restapi.utils.TokenUtils;
 import cu.uci.coj.utils.paging.IPaginatedList;
 import cu.uci.coj.utils.paging.PagingOptions;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.annotation.Resource;
-import org.apache.commons.io.FileUtils;
-import org.json.zip.JSONzip;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
